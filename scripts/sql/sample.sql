e0ce926e4e351a86636b6a8126d63e5b386b1ae7
Biniyam Belayneh <biniyambelayneh376@gmail.com>
2022-07-19 14:19:38 -0700

    create table from datframe

--- scripts/sql/sample.sql
+++ scripts/sql/sample.sql
@@ -1,14 +1,1 @@
-CREATE TABLE if not exists airports (
-    iata text PRIMARY KEY,
-    city text,
-    country text
-);
-
-INSERT INTO airports
-VALUES
-    ("ORD", "Chicago", "United States"),
-    ("JFK", "New York City", "United States"),
-    ("CDG", "Paris", "France"),
-    ("LHR", "London", "United Kingdom"),
-    ("DME", "Moscow", "Russia"),
-    ("SVO", "Moscow", "Russia")
+CREATE TABLE IF NOT EXISTS products (product_name text, price number)
